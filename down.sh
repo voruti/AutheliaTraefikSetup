2480a810c77413e648955c9cf53fd5191b9b0622
voruti <45130096+voruti@users.noreply.github.com>
2024-01-14 00:48:44 +0100

    Remove scripts

--- down.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-docker compose -f ./host1/docker-compose.yml down
-docker compose -f ./host2/docker-compose.yml down
--- up.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-
-docker compose -f ./host1/docker-compose.yml up -d
-docker compose -f ./host2/docker-compose.yml up -d
