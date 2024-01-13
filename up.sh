c40080e3fc54458fa6e53490a44b76e6400e9059
voruti <45130096+voruti@users.noreply.github.com>
2024-01-13 23:18:38 +0100

    Traefik on host1

--- up.sh
+++ up.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-docker compose -f ./host1/authelia/docker-compose.yml up -d
-docker compose -f ./host1/traefik/docker-compose.yml up -d
-docker compose -f ./host1/service/docker-compose.yml up -d
+docker compose -f ./host1/docker-compose.yml up -d
+docker compose -f ./host2/docker-compose.yml up -d
